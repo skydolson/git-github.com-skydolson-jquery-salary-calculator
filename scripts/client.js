5318e92461fb9b3c71de6e0b18532827749f6638
skydolson <skyolson@gmail.com>
2022-11-20 16:25:23 -0600

    testing submitEmployee function

--- scripts/client.js
+++ scripts/client.js
@@ -1,5 +1,30 @@
 $( document ).ready(onReady);
 
+const budget = 20000;
+
+let employees = [];
+
 function onReady() {
-    console.log( "JQ" );
+    let el = $('#budgetOut');
+    el.empty();
+    el.append(budget);
+    $('#submitButton').on('click', submitEmployee);
+}
+
+function submitEmployee(){
+    console.log("in submitEmployee");
+    // get inputs, create object, push object, empty inputs, calculate surplus
+    let newEmployee = {
+        firstName: $('#firstNameIn').val(),
+        lastName: $('#lastNameIn').val(),
+        idNumber: $('#idNumberIn').val(),
+        jobTitle: $('#jobTitleIn').val(),
+        annualSalary: $('#annualSalaryIn').val(),
+    }
+    employees.push( newEmployee);
+    $('#firstNameIn').val('');
+    $('#lastNameIn').val('');
+    $('#idNumberIn').val('');
+    $('#jobTitleIn').val('');
+    $('#annualSalaryIn').val('');
 }
