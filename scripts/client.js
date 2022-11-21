72b00efd1492786cca4bc46e14a8bb122da69d8c
skydolson <skyolson@gmail.com>
2022-11-21 13:20:09 -0600

    Working on adding red background

--- scripts/client.js
+++ scripts/client.js
@@ -9,6 +9,11 @@ function onReady() {
     el.empty();
     el.append(budget);
     $('#submitButton').on('click', submitEmployee);
+    $('#deleteButton').on('click', deleteEmployee);
+    $('#deleteButton').on('click', showEmployees);
+    findSurplus();
+    
+
 }
 
 function submitEmployee(){
@@ -21,10 +26,55 @@ function submitEmployee(){
         jobTitle: $('#jobTitleIn').val(),
         annualSalary: $('#annualSalaryIn').val(),
     }
-    employees.push( newEmployee);
+    employees.push(newEmployee);
+
     $('#firstNameIn').val('');
     $('#lastNameIn').val('');
     $('#idNumberIn').val('');
     $('#jobTitleIn').val('');
     $('#annualSalaryIn').val('');
+
+    findSurplus();
+    showEmployees();
 }
+
+function findSurplus (){
+    // loop over employees adding their salaries to total, then subtract total from budget.
+    totalSalaries = 0
+    
+    for (employee of employees) {
+        totalSalaries += Number(employee.annualSalary)/12;
+        
+    }
+    console.log("total salaries:", totalSalaries);
+    console.log("surplus:", budget - totalSalaries);
+    
+    let el1 = $('#salariesOut');
+    el1.empty();
+    el1.append(totalSalaries);
+    
+    const surplus = budget - totalSalaries;
+    let el2 = $('#surplusOut');
+    el2.empty(); 
+    el2.append(surplus);
+
+    if (totalSalaries > budget) {
+        console.log("Too big!")
+    }
+ }
+
+ function showEmployees(){
+    let el = $('#employeesOut');
+    el.empty();
+    for (employee of employees){
+        el.append( `<li>` + employee.firstName + ` ` + employee.lastName + 
+        `, ID Number: ` + employee.idNumber + `, Job Title: ` + employee.jobTitle
+         + `, Annual Salary: ` + employee.annualSalary + `<li>`);
+    }
+
+ }
+
+ function deleteEmployee(){
+    employees.pop();
+
+ }
