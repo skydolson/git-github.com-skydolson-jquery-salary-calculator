efddc2402ee06a3679ee64e1013962a8f11fe296
skydolson <skyolson@gmail.com>
2022-11-21 13:57:21 -0600

    Base mode complete

--- scripts/client.js
+++ scripts/client.js
@@ -11,14 +11,12 @@ function onReady() {
     $('#submitButton').on('click', submitEmployee);
     $('#deleteButton').on('click', deleteEmployee);
     $('#deleteButton').on('click', showEmployees);
+    $('#deleteButton').on('click', findSurplus);
     findSurplus();
-    
-
 }
 
 function submitEmployee(){
-    console.log("in submitEmployee");
-    // get inputs, create object, push object, empty inputs, calculate surplus
+    // get inputs, create object, push object, empty inputs, calculate surplus, display employees
     let newEmployee = {
         firstName: $('#firstNameIn').val(),
         lastName: $('#lastNameIn').val(),
@@ -39,27 +37,30 @@ function submitEmployee(){
 }
 
 function findSurplus (){
-    // loop over employees adding their salaries to total, then subtract total from budget.
+    // loop over employees adding their salaries to total, then subtract total from budget
+    // apply red background to over-budget condition
     totalSalaries = 0
-    
     for (employee of employees) {
         totalSalaries += Number(employee.annualSalary)/12;
-        
     }
-    console.log("total salaries:", totalSalaries);
-    console.log("surplus:", budget - totalSalaries);
-    
+    let totalSalariesTwoDecimalPlaces = Math.round((totalSalaries + Number.EPSILON) * 100) / 100;
     let el1 = $('#salariesOut');
     el1.empty();
-    el1.append(totalSalaries);
+    el1.append(totalSalariesTwoDecimalPlaces);
     
     const surplus = budget - totalSalaries;
+    let surplusTwoDecimalPlaces = Math.round((surplus + Number.EPSILON) * 100) / 100;
     let el2 = $('#surplusOut');
     el2.empty(); 
-    el2.append(surplus);
+    el2.append(surplusTwoDecimalPlaces);
 
     if (totalSalaries > budget) {
-        console.log("Too big!")
+        document.getElementById("salariesOut").style.backgroundColor = 'Red';
+        document.getElementById("surplusOut").style.backgroundColor = 'Red';
+    }
+    if (totalSalaries <= budget) {
+        document.getElementById("salariesOut").style.backgroundColor = 'White';
+        document.getElementById("surplusOut").style.backgroundColor = 'White';
     }
  }
 
@@ -67,14 +68,12 @@ function findSurplus (){
     let el = $('#employeesOut');
     el.empty();
     for (employee of employees){
-        el.append( `<li>` + employee.firstName + ` ` + employee.lastName + 
+        el.append( `<li>` + `Full Name: ` + employee.firstName + ` ` + employee.lastName + 
         `, ID Number: ` + employee.idNumber + `, Job Title: ` + employee.jobTitle
-         + `, Annual Salary: ` + employee.annualSalary + `<li>`);
+         + `, Annual Salary: $` + employee.annualSalary + `<li>`);
     }
-
  }
 
  function deleteEmployee(){
     employees.pop();
-
  }
